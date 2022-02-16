87b06247c267936259102b086fcdf8859091e394
Gustavo Antunes <gustavoamalheiros@gmail.com>
2022-02-16 20:57:05 -0300

    Initial Commit

---
+++ KivaAPI/KivaAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  KivaAPIApp.swift
+//  KivaAPI
+//
+//  Created by Gustavo Malheiros on 16/02/22.
+//
+
+import SwiftUI
+
+@main
+struct KivaAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
