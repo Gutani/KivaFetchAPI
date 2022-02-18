96020cbd9fd8cae99a1983ac9a645335b81d2370
Gustavo Antunes <gustavoamalheiros@gmail.com>
2022-02-18 13:23:07 -0300

    BottomSheet Package (from https://github.com/LucasMucGH/BottomSheet) to Filter added, Refreshable rate added.

--- KivaAPI/View/ContentView.swift
+++ KivaAPI/View/ContentView.swift
@@ -6,38 +6,100 @@
 //
 
 import SwiftUI
+import BottomSheet
+
+public enum BottomSheetPositions: CGFloat, CaseIterable {
+    case middle = 0.4, bottom = 0.1
+}
 
 struct ContentView: View {
     @ObservedObject var loanStore = LoanStore()
     @State private var filterEnabled = false
-    @State private var maximumLoanAmount = 10000.0
+    @State private var maximumLoanAmount = 5000.0
+    
+    @State var amount: Double = 5000.0
+    var minAmount = 0.0
+    var maxAmount = 5000.0
+    
+    
+    @State var bottomSheetPosition: BottomSheetPositions = .middle
     
     var body: some View {
         NavigationView {
-            if filterEnabled {
-                LoanFilterView(amount: $maximumLoanAmount).transition(.opacity)
-            }
-            List{
-                ForEach(loanStore.loans) { loan in
-                    LoanCellView(loan: loan)
+            
+            ZStack {
+                List{
+                    ForEach(loanStore.loans) { loan in
+                        LoanCellView(loan: loan)
+                    }
+                }.refreshable {
+                    self.loanStore.fetchLoans()
                 }
-            }
-            .listStyle(.plain)
-            .onAppear {
-                self.loanStore.fetchLoans()
-            }
-            .navigationTitle("Kiva API Fetch")
-        }
-        .toolbar {
-            Button {
-                withAnimation(.linear) {
-                    filterEnabled.toggle()
-                    loanStore.filterLoans(amount: Int(maximumLoanAmount))
+                .listStyle(.plain)
+                .bottomSheet(bottomSheetPosition: self.$bottomSheetPosition , options: [.background(AnyView(Color.gray.opacity(0.9))), .noBottomPosition, .cornerRadius(30)] ,headerContent: {
+                    VStack {
+                        Text("Filter the value you want to show").bold().font(.title2)                                    .foregroundColor(.white)
+
+                        Divider()
+                        Text("Showing loan amount below $ \(Int(amount))")
+                            .font(.system(.headline, design: .rounded))
+                            .padding()
+                            .foregroundColor(.white)
+
+                    }
+                    
+                    
+                }) {
+                    
+                    if bottomSheetPosition == .middle {
+                        VStack(alignment: .leading) {
+                            
+                            HStack {
+                                Slider(value: $amount, in: minAmount...maxAmount, step: 100)
+                                    .accentColor(Color(red: 0.28, green: 0.28, blue: 0.53))
+                                    .foregroundColor(.white)
+                            }
+                            
+                            HStack {
+                                Text("\(Int(minAmount))")
+                                    .font(.system(.footnote, design: .rounded))
+                                Spacer()
+                                Text("\(Int(maxAmount))")
+                                    .font(.system(.footnote, design: .rounded))
+                            }.foregroundColor(.white)
+
+                            
+                            Button {
+                                self.loanStore.filterLoans(amount: Int(amount))
+                            } label: {
+                                Text("filter".uppercased())
+                                    .frame(maxWidth: .infinity)
+                                    .padding()
+                                    .foregroundColor(.white)
+                                    .background(Color(red: 0.28, green: 0.28, blue: 0.53))
+                                    .cornerRadius(10)
+                                    .padding(.horizontal)
+                            }
+                        }
+                        .padding(.horizontal)
+                        .transition(.opacity)
+                        .foregroundColor(.white)
+                    }
+                    
+                    
                 }
-            } label: {
-                Text("Filter").foregroundColor(.primary).font(.subheadline)
+                
+                
+                
             }
+            .navigationTitle("Kiva API Fetch").navigationBarTitleDisplayMode(.inline).foregroundColor(Color(red: 0.28, green: 0.28, blue: 0.53))
+            
         }
+        .navigationViewStyle(StackNavigationViewStyle())
+        .onAppear {
+            self.loanStore.fetchLoans()
+        }
+        
     }
     
 }
